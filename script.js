e41d3e41e343b13816a8bba2db188eceff337141
João Guilherme <joaogacanuto@gmail.com>
2023-11-27 20:05:30 -0300

    fix

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
   const friendMap = {
-    'Dayse': '13df3cd3',
-    'João': '9c483934',
-    'Gustavo': 'ec29b76d',
-    'Daiana': 'e401c458',
-    'Daniel': '7297db81',
-    'Socorro': 'af5f5e0f',
-    'Flávio': 'c1fee031',
-    'Alice': '3bc51062'
+    '13df3cd3': 'Dayse',
+    '9c483934': 'João',
+    'ec29b76d': 'Gustavo',
+    'e401c458': 'Daiana',
+    '7297db81': 'Daniel',
+    'af5f5e0f': 'Socorro',
+    'c1fee031': 'Flávio',
+    '3bc51062': 'Alice'
 };
 
   const params = new URLSearchParams(window.location.search);
-  const id = params.get('id');
+  const id = params.get('id').toString();
   const friendName = friendMap[id];
+  console.log(friendName);
 
   if (friendName) {
       document.getElementById('secretFriend').innerText = `Parabéns! Seu amigo secreto é ${friendName}.`;
