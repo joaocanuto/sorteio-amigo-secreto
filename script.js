1103145e8b9f4e107a7fd3191eaa8e2c581e6fe5
João Guilherme <joaogacanuto@gmail.com>
2023-11-27 19:55:59 -0300

    updates no map

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   const friendMap = {
-      1: 'Dayse',
-      2: 'João',
-      3: 'Gustavo',
-      4: 'Daiana',
-      5: 'Daniel',
-      6: 'Socorro',
-      7: 'Flávio',
-      8: 'Alice'
-  };
+    'Dayse': '13df3cd3',
+    'João': '9c483934',
+    'Gustavo': 'ec29b76d',
+    'Daiana': 'e401c458',
+    'Daniel': '7297db81',
+    'Socorro': 'af5f5e0f',
+    'Flávio': 'c1fee031',
+    'Alice': '3bc51062'
+};
 
   const params = new URLSearchParams(window.location.search);
   const id = params.get('id');
